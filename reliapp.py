bef0c45d1fe757beb0d3c0680b3a1c6edf4eeb80
Rafael Schena <rafaelschena@MacBook-Air-de-Rafael.local>
2024-11-24 16:11:12 -0300

    Layout adjustments

--- reliapp.py
+++ reliapp.py
@@ -5,13 +5,15 @@ import plotly.graph_objects as go
 import plotly.express as px
 from utils import *
 import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
+
 
 
 def plot_line(df, col):
     fig = px.line(
                     df,
                     x=df.index,
-                    y=df[col],
+                    y=col,
                     # labels={"value": "Valor", "variable": "Features"},
                     # title=f"{col} temporal evolution"
                     )
@@ -59,10 +61,11 @@ def on_change(state, var_name, var_value):
         state.data_chart_2 = senoidal_data(freq=state.freq_chart_2)
         return
     elif var_name == "idx":
-        state.df = state.df_orig[:int(state.idx)]
+        state.df = state.df_scaled[:int(state.idx)]
         # state.delta_series = state.df.iloc[-1] - state.df.iloc[-2]
-        state.dic_figs = {col: plot_line(state.df, col) for col in state.df.columns}
+        # state.dic_figs = {col: plot_line(state.df, col) for col in state.df.columns}
         state.dic_gauges = {col: create_gauge(state.df[col]) for col in state.df.columns}
+        state.plot_page_1 = plot_line(state.df, ['lp', 'v', 'P48', 'T48'])
         return
 
 def senoidal_data(freq=1, ampl=1, phase=0):
@@ -72,110 +75,54 @@ def senoidal_data(freq=1, ampl=1, phase=0):
 ## Plotly charts
 df_orig = load_data_naval_pdm()
 idx=100
-df = df_orig[:idx]
+scaler = MinMaxScaler()
+scaled_data = scaler.fit_transform(df_orig)
+df_scaled = pd.DataFrame(scaled_data, columns=df_orig.columns)
+df = df_scaled[:idx]
 # delta_series = df.iloc[-1] - df.iloc[-2]
-dic_figs = {col: plot_line(df, col) for col in df.columns}
+# dic_figs = {col: plot_line(df, col) for col in df.columns}
 dic_gauges = {col: create_gauge(df[col]) for col in df.columns}
+plot_page_1 = plot_line(df, ['lp', 'v', 'P48', 'T48'])
 
 show_pane = True
 
 ### Layout da página
 page = """
 <|toggle|theme|>
-
 <|layout|columns=300px 1fr|
-
 <|padding=10px|
 ### Change the number to simulate time elapsing:
 <|{idx}|number|min=100|max={df_orig.index.max()}|>
 |>
-
-<|padding=50px|
+<|layout|columns=1fr|
 # ReliApp - Predictive Maintenance and Reliability Indicators
-<|layout|columns=1 1|
-
+<|layout|columns=1 1 1 1|
 <|part|render=True|
 ### lp - Lever position
 <|chart|figure={dic_gauges['lp']}|height=300px|>
-
-### lp history
-<|chart|figure={dic_figs['lp']}|height=300px|>
 |>
-
+<|part|render=True|
+### v - ship speed
+<|chart|figure={dic_gauges['v']}|height=300px|>
+|>
 <|part|render=True|
 ### P48 - HP exit pressure
 <|chart|figure={dic_gauges['P48']}|height=300px|>
-
-### P48 - History
-<|chart|figure={dic_figs['P48']}|height=300px|>
 |>
-
+<|part|render=True|
+### T48 - HP exit temp
+<|chart|figure={dic_gauges['T48']}|height=300px|>
 |>
 |>
-
+  \
+  \
+<|part|render=True|
+<|chart|figure={plot_page_1}|height=300px|>
+|>
+|>
 |>
 """
 
 if __name__ == "__main__":
 
     Gui(page=page).run(port=8000, host="0.0.0.0", favicon='wrench.png', title="ReliApp", watermark='')
-
-'''
-<|{show_pane}|pane|anchor=left|width=220|active=False|>
-Change the number to simulate the time elapsing:
-<|{idx}|number|min=100|max={df_orig.index.max()}|>
-|>
-
-## Slider
-value_slider = 9
-
-## Color maps
-color_map = {
-    0: "red",
-    30: "orange",
-    60: "yellow",
-    80: "green"
-}
-
-
-## Gauge
-value_gauge = 72
-threshold = 60
-delta = value_gauge - threshold
-
-<|{value_gauge}|slider|min=0|max=100|step=1|>
-<|{show_pane}|pane|anchor=left|width=200|active=True|
-|>
-
-<|part|render=True|
-### P48 - HP exit pressure
-<|{df['P48'].values[-1]}|metric|delta={delta_series['P48']}|bar_color=darkgoldenrod|min={df['P48'].min()}|max={df['P48'].max()}|>
-|>
-
-<|part|render=True|
-### T48 - HP exit temperature
-<|{df['T48'].values[-1]}|metric|delta={delta_series['T48']}|bar_color=darkgoldenrod|min={df['T48'].min()}|max={df['T48'].max()}|>
-|>
-
-<|part|render=True|
-### P48 history
-<|chart|figure={dic_figs['P48']}|>
-|>
-<|part|render=True|
-### T48 history
-<|chart|figure={dic_figs['T48']}|>
-|>
-
-<|layout|columns=1 1|
-
-<|part|render=True|
-### v - Ship speed
-<|{df['v'].values[-1]}|metric|delta={delta_series['v']}|bar_color=darkgoldenrod|min={df['v'].min()}|max={df['v'].max()}|>
-|>
-
-<|part|render=True|
-### v history
-<|chart|figure={dic_figs['v']}|>
-|>
-
-'''
